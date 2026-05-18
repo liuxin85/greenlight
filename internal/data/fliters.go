a202c31ece3a76e005ba7baead14554ec9a2410b
liuxin85 <liuxin85@gmail.com>
2026-05-18 19:33:13 +0800

    9.6 sorting list

--- internal/data/fliters.go
+++ internal/data/fliters.go
@@ -1,16 +1,20 @@
 package data
 
-import "greenlight.alexedwards.net/internal/validator"
+import (
+	"strings"
+
+	"greenlight.alexedwards.net/internal/validator"
+)
 
 // Add a SortSafelist field to hold the supported sort vlaues.
 type Filters struct {
-	Page     int
-	PageSize int
-	Sort     string
+	Page         int
+	PageSize     int
+	Sort         string
 	SortSafelist []string
 }
 
-func ValidateFilters(v *validator.Validator, f Filters){
+func ValidateFilters(v *validator.Validator, f Filters) {
 	v.Check(f.Page > 0, "page", "must be greater than zero")
 	v.Check(f.Page <= 10_000_000, "page", "must be a maximum of 10 million")
 	v.Check(f.PageSize > 0, "page_size", "must be greater than zero")
@@ -20,3 +24,18 @@ func ValidateFilters(v *validator.Validator, f Filters){
 	v.Check(validator.PermittedValue(f.Sort, f.SortSafelist...), "sort", "invalid sort value")
 }
 
+func (f Filters) sortColumn() string {
+	for _, safeValue := range f.SortSafelist {
+		if f.Sort == safeValue {
+			return strings.TrimPrefix(f.Sort, "-")
+		}
+	}
+	panic("unsafe sort parameter: " + f.Sort)
+}
+
+func (f Filters) sortDirection() string {
+	if strings.HasPrefix(f.Sort, "-") {
+		return "DESC"
+	}
+	return "ASC"
+}
