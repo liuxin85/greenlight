5a02562f850313b10884f270216b26b10f56cc2c
liuxin85 <liuxin85@gmail.com>
2026-06-06 19:20:15 +0800

    10.2 ip based rate limiting

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"fmt"
+	"net"
 	"net/http"
+	"sync"
+	"time"
 
-	"golang.org/x/time/rate" // New import
+	"golang.org/x/time/rate"
 )
 
 func (app *application) recoverPanic(next http.Handler) http.Handler {
@@ -22,17 +25,64 @@ func (app *application) recoverPanic(next http.Handler) http.Handler {
 }
 
 func (app *application) rateLimit(next http.Handler) http.Handler {
-	// allows an average of 2 requests per second,
-	// with a maximum of 4 requests in a single "burst".
-	limiter := rate.NewLimiter(2, 4)
+	type client struct {
+		limiter  *rate.Limiter
+		lastSeen time.Time
+	}
+	// Delare a mutex and a map to hold the client's IP addresses and rate limiters.
+	var (
+		mu      sync.Mutex
+		clients = make(map[string]*client)
+	)
+
+	// Lauch a background goroutine which remove old entries
+	go func() {
+		for {
+			time.Sleep(time.Minute)
+			mu.Lock()
+			for ip, client := range clients {
+				if time.Since(client.lastSeen) > 3*time.Minute {
+					delete(clients, ip)
+				}
+			}
+			mu.Unlock()
+
+		}
+	}()
 
 	//  The function we are returning is a closure, which 'closes over' the limiter
 	// variable.
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if !limiter.Allow() {
+
+		// Extrhact the client's IP address from the request.
+		ip, _, err := net.SplitHostPort(r.RemoteAddr)
+
+		if err != nil {
+			app.serverErrorResponse(w, r, err)
+			return
+		}
+		// Lock the mutex to prevent this code from being executed concurrently.
+		mu.Lock()
+
+		if _, found := clients[ip]; !found {
+			// Create and add a new client struct to the map if it doesn't already exist.
+			clients[ip] = &client{limiter: rate.NewLimiter(2, 4)}
+		}
+		// Update the last seen time for the client.
+		clients[ip].lastSeen = time.Now()
+
+		if !clients[ip].limiter.Allow() {
+			mu.Unlock()
 			app.rateLimitExeededResponse(w, r)
 			return
 		}
+
+		// Very importantly, unlock the mutex before calling the next handler in the
+		// chain. Notice that we DON't use defer to unlock the mutext, as that would mean
+		// that the mutex isn't unlocked until all the handlers dowstream of this
+		// middleware have also returned
+		mu.Unlock()
+
 		next.ServeHTTP(w, r)
 	})
 }
